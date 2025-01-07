0245f285559133bad38ecf859555217848c7c27c
Penumbra <xxsaykexx@gmail.com>
2025-01-07 23:57:27 +0200

    Create Companion.gd

---
+++ scripts/Companion.gd
@@ -0,0 +1,22 @@
+class_name Companion extends Resource
+
+@export var name:String
+@export var nickname:String:
+	get:
+		if nickname:
+			return nickname
+		else:
+			return name
+@export_enum("Fox Humanoid") var race:String #TODO: Implement Race resource
+@export_group("Personality")
+
+@export_group("Background")
+@export var background:String: #TODO: Implement Background resource
+	set(new_background):
+		#Safety net, avoid setting the background if it is not available
+		if not new_background in available_backgrounds:
+			return
+		else:
+			background = new_background
+#Determines what backgrounds the companion is eligible for.
+@export var available_backgrounds:Array[String] #TODO: Change to Array[Background] when implemented
